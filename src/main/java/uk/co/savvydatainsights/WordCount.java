9d256dd70d3ab566e8db2e403dc819b9a74ec1ef
Gustavo Muniz do Carmo <gmunizcarmo@aubay.com>
2019-05-24 09:43:58 +0100

    Spark SQL SparkSession

--- src/main/java/uk/co/savvydatainsights/WordCount.java
+++ src/main/java/uk/co/savvydatainsights/WordCount.java
@@ -1,9 +1,9 @@
 package uk.co.savvydatainsights;
 
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
 import java.util.Arrays;
@@ -11,19 +11,19 @@ import java.util.List;
 
 public class WordCount {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         if (args.length < 1) {
             System.err.println("Usage: WordCount <file>");
             System.exit(1);
         }
-        SparkConf sparkConf = new SparkConf().setAppName("WordCount");
-        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+        SparkSession spark = SparkSession.builder().appName("WordCount").getOrCreate();
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
-        JavaRDD<String> textFile = ctx.textFile(args[0], 1);
+        JavaRDD<String> textFile = jsc.textFile(args[0], 1);
         JavaPairRDD<String, Integer> counts = textFile
-            .flatMap(s -> Arrays.asList(s.replaceAll("[^a-zA-Z ]", "").split("\\s+")).iterator())
-            .mapToPair(word -> new Tuple2<>(word, 1))
-            .reduceByKey((a, b) -> a + b);
+                .flatMap(s -> Arrays.asList(s.replaceAll("[^a-zA-Z ]", "").split("\\s+")).iterator())
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey((a, b) -> a + b);
 
         List<Tuple2<String, Integer>> output = counts.collect();
         for (Tuple2<String, Integer> tuple : output) {
@@ -33,7 +33,7 @@ public class WordCount {
         if (args.length > 1) {
             counts.saveAsTextFile(args[1]);
         }
-        ctx.stop();
+        jsc.stop();
     }
 
 }
